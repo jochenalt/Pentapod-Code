19e33ba76861deb18461a10efbe9babb5ec26b75
jochen.alt@gmx.net <jochen.alt@gmx.net>
2017-12-16 23:47:32 +0100

    timing

--- Kinematics/src/I2CMaster.cpp
+++ Kinematics/src/I2CMaster.cpp
@@ -83,7 +83,7 @@ int I2CMaster::receiveArray(uint8_t* buffer, int RemainingBufferSize, int timeou
     	bytesRead= getArray(&buffer[totalBytesRead], RemainingBufferSize);
     	if ((bytesRead > 0) && (buffer[totalBytesRead] ==  Cortex::NotYetReadyMagicNumber)) {
         	bytesRead = 0;
-        	delay_ms(1);
+        	delay_ms(2);
         	tries++;
     	}
     	if (bytesRead > 0) {
